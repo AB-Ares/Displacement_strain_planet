0ed536b432b8047ece48c1ba77cf11f4bb8b98d4
Adrien Broquet <adrien.broquet@oca.eu>
2021-09-15 13:14:31 -0700

    Update setup.py
    Add numpy as matplotlib in the setup requirements

--- setup.py
+++ setup.py
@@ -10,7 +10,14 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
-install_requires = ["pyshtools>=4.7.1", "sympy<=1.7", "cmcrameri", "jupyter"]
+install_requires = [
+    "pyshtools>=4.7.1",
+    "sympy<=1.7",
+    "matplotlib>=3.3",
+    "numpy",
+    "cmcrameri",
+    "jupyter",
+]
 
 setup(
     name="Displacement_strain_planet",
